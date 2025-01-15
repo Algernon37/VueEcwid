9706d99f1af6e19cccad1684a875f7b89f27de7c
Ivan Esin <ivan.esin.78@mail.ru>
2025-01-15 19:54:47 +0400

    first commit

---
+++ vueecwid/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import HeaderComponent from "../src/components/HeaderComponent.vue";
+    import CardList from "./components/CardList.vue";
+</script>
+
+<template>
+ <div class="bg-white w-4/5 m-auto  rounded-xl shadow-xl mt-14">
+    <HeaderComponent />  
+    <CardList />
+ </div>
+</template>
