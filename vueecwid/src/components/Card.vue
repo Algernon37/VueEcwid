9d41f4309a5d5b096d844d9fa15fc715c375a976
Ivan Esin <ivan.esin.78@mail.ru>
2025-01-16 17:43:04 +0400

    first commit

--- vueecwid/src/components/Card.vue
+++ vueecwid/src/components/Card.vue
@@ -1,14 +1,25 @@
+<script setup>
+    defineProps({
+        imageURL: String,
+        title: String,
+        price: Number,
+        isFavorite: Boolean,
+        isAdded: Boolean,
+        onClickAdd: Function,
+        onClickFavorite: Function,
+    })
+</script>
 <template>
     <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-        <img src="/like-1.svg" alt="like 1" class="absolute top-0 left-0"/>
-        <img src="/sneakers/sneakers-1.jpg" alt="sneakers" />
-        <p class="mt-2">Мужские кросовки</p>
+        <img @click="onClickFavorite" :src="isFavorite ? '/like-1.svg' : '/like-2.svg'" class="absolute top-0 left-0" alt="like" />
+        <img :src="imageURL" alt="sneakers" />
+        <p class="mt-2">{{ title }}</p>
         <div class="flex justify-between mt-5">
             <div class="flex flex-col">
                 <span class="text-slate-400">Цена:</span>
-                <b>12 599</b>
+                <b>{{ price }}</b>
             </div>
-            <img src="/plus.svg" alt="plus" />
+            <img @click="onClickAdd" :src="isAdded ? '/plus.svg' : '/checked.svg'" alt="plus" />
         </div>
     </div>
 </template>
--- vueecwid/src/components/CardList.vue
+++ vueecwid/src/components/CardList.vue
@@ -1,14 +1,22 @@
+/* eslint-disable */
 <script setup>
     import Card from "./Card.vue";
+
+    
 </script>
 <template>
         <div class="p-10">
         <h2 class="text-3xl font-bold mb-8">All items</h2>
         <div class="grid grid-cols-4 gap-5">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card 
+                title="Кроссы" 
+                image-url=""
+                :price="12900"
+                :isAdded="true"
+                :isFavorite="false"
+                :onClickAdd=""
+                :onClickFavorite=""
+            />
         </div>
     </div>
 </template>
